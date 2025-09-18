dc650d233ff129a9a376ee2a8723b720aa13775d
Felipe Alvarado <devfelipe663@gmail.com>
2025-09-18 17:10:14 -0500

    feat: add project

---
+++ src/components/day0.js
@@ -0,0 +1,10 @@
+const compDay0 = `
+  <h1>Regalos</h1>
+  <ul>
+    <li>Medias</li>
+    <li>Caramelos</li>
+    <li>Vintel Tone</li>
+  </ul>
+`;
+
+export { compDay0 };
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './style.css'
+import { compDay0 } from './components/day0';
+
+document.querySelector('#app').innerHTML = compDay0;
